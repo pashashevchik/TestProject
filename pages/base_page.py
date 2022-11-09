401984d468e7112231ebef45f246a65b04644ceb
pashashevchik <pshevchik55@mail.ru>
2022-11-09 17:01:21 +0300

    added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait as wait
+import allure
+
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 
 class BasePage:
@@ -9,31 +10,70 @@ class BasePage:
         self.driver = driver
         self.url = url
 
+    @allure.step('Open a browser')
     def open(self):
-        """ Open url """
         self.driver.get(self.url)
 
-    #Method get element
-    def     get_element(self, locator, timeout=10, select_method="clickable"):
-        """ Gets the element  """
-        return wait(self.driver, 10).until(EC.element_to_be_clickable((locator)))
-        if select_method == "clickable":
-            return wait(self.driver, 10).until(EC.element_to_be_clickable((locator)))
-        elif select_method == "selected":
-            return wait.until(EC.element_to_be_selected((locator)))
-        elif select_method == "visibility_located":
-            return wait.until(EC.visibility_of_element_located((locator)))
-        elif select_method == "visibility_of_all_located":
-            return wait.until(EC.visibility_of_all_elements_located((locator)))
-        elif select_method == "presence_element":
-            return wait.until(EC.presence_of_element_located((locator)))
-        elif select_method == "presence_of_all_element":
-            return wait.until(EC.presence_of_all_elements_located((locator)))
-        elif select_method == "element_not_visible":
-            return wait.until(EC.invisibility_of_element_located((locator)))
+    @allure.step('Find a visible element')
+    def element_is_visible(self, locator, timeout=5):
+        self.go_to_element(self.element_is_present(locator))
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
-    #Method go to element
+    @allure.step('Find visible elements')
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    @allure.step('Find a present element')
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    @allure.step('Find present elements')
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    @allure.step('Find a not visible element')
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+
+    @allure.step('Find clickable elements')
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    @allure.step('Go to specified element')
     def go_to_element(self, element):
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
+    @allure.step('Double click')
+    def action_double_click(self, element):
+        action = ActionChains(self.driver)
+        action.double_click(element)
+        action.perform()
 
+    @allure.step('Right click')
+    def action_right_click(self, element):
+        action = ActionChains(self.driver)
+        action.context_click(element)
+        action.perform()
+
+    @allure.step('Drag and drop by offset')
+    def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
+        action = ActionChains(self.driver)
+        action.drag_and_drop_by_offset(element, x_coords, y_coords)
+        action.perform()
+
+    @allure.step('Drag and drop element to element')
+    def action_drag_and_drop_to_element(self, what, where):
+        action = ActionChains(self.driver)
+        action.drag_and_drop(what, where)
+        action.perform()
+
+    @allure.step('Move cursor to element')
+    def action_move_to_element(self, element):
+        action = ActionChains(self.driver)
+        action.move_to_element(element)
+        action.perform()
+
+    @allure.step('Remove footer')
+    def remove_footer(self):
+        self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
+        self.driver.execute_script("document.getElementsById('close-fixedban').remove();")
